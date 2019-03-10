44260cc039d76c4aa7d25790b4c1c3bd55441010
Aravind Reddy V <zerodividedby0@gmail.com>
2019-03-10 22:08:44 +0530

    fix indent

--- assignments/2/echoServer/client.c
+++ assignments/2/echoServer/client.c
@@ -25,58 +25,58 @@ int client_fd;
 int main() {
 
 
-	/**
+    /**
 
-	@ Creating a socket.
+    @ Creating a socket.
 
-	- int
-	  socket(int domain, int type, int protocol);
-	- man 2 socket
+    - int
+      socket(int domain, int type, int protocol);
+    - man 2 socket
 
-	@ Arguments and their meaning.
+    @ Arguments and their meaning.
 
-	^ PF_LOCAL means the socket is for local communication,
-	  and is not a TCP socket for external communication.
+    ^ PF_LOCAL means the socket is for local communication,
+      and is not a TCP socket for external communication.
 
-	^ SOCK_STREAM makes the socket type 'stream'
-	  other types are datagram (udp) and
-	  raw (full control over internals)
-	  read the man pages
+    ^ SOCK_STREAM makes the socket type 'stream'
+      other types are datagram (udp) and
+      raw (full control over internals)
+      read the man pages
 
-	^ The last argument is for protocol.
-	  There is _usually_ just one protocol.
-	  0 is the first one. Arrays start with 0, right?
+    ^ The last argument is for protocol.
+      There is _usually_ just one protocol.
+      0 is the first one. Arrays start with 0, right?
 
-	^ Works on Macos (BSD, System V)
-	  May have to change the arguments for other systems.
-	  for example, AF_LOCAL. See man pages.
+    ^ Works on Macos (BSD, System V)
+      May have to change the arguments for other systems.
+      for example, AF_LOCAL. See man pages.
 
-	**/
-	if ( (client_fd = socket(PF_LOCAL, SOCK_STREAM, 0)) == -1 ) {
-		perror("Socket creation failed.");
-		exit(EXIT_FAILURE);
-	}
+    **/
+    if ( (client_fd = socket(PF_LOCAL, SOCK_STREAM, 0)) == -1 ) {
+        perror("Socket creation failed.");
+        exit(EXIT_FAILURE);
+    }
 
 
-	struct sockaddr_un address;
+    struct sockaddr_un address;
 
     address.sun_family = PF_LOCAL;
     strcpy(address.sun_path, SERVER_PATH);
 
     if(connect(client_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
-    	perror("An error occured while connecting to server.");
-    	exit(EXIT_FAILURE);
+        perror("An error occured while connecting to server.");
+        exit(EXIT_FAILURE);
     }
 
     char letter = 'A';
     if(write(client_fd, &letter, sizeof(char)) == -1) {
-    	perror("Error writing to server.");
-    	exit(EXIT_FAILURE);
+        perror("Error writing to server.");
+        exit(EXIT_FAILURE);
     }
 
     // if(read(client_fd, &letter, sizeof(char)) == -1) {
-    // 	perror("Error reading from server.");
-    // 	exit(EXIT_FAILURE);
+    //  perror("Error reading from server.");
+    //  exit(EXIT_FAILURE);
     // }
 
     shutdown(client_fd, SHUT_RDWR);
--- assignments/2/echoServer/server.c
+++ assignments/2/echoServer/server.c
@@ -39,103 +39,104 @@ int server_fd;
 
 void sighandler(int sig_num) {
 
-	shutdown(server_fd, SHUT_RDWR);
-	close(server_fd);
+  shutdown(server_fd, SHUT_RDWR);
+  close(server_fd);
+
   unlink(SERVER_PATH);
 
-	exit(sig_num);
+  exit(sig_num);
 }
 
 
 int main() {
 
-	signal(SIGINT,  sighandler); // Interrupt
-	signal(SIGTSTP, sighandler); // Stop from keyboard
-	signal(SIGTERM, sighandler); // Software term signal
+  signal(SIGINT,  sighandler); // Interrupt
+  signal(SIGTSTP, sighandler); // Stop from keyboard
+  signal(SIGTERM, sighandler); // Software term signal
 
-	signal(SIGQUIT, sighandler); // Quit
-	signal(SIGILL,  sighandler); // Illegal instruction
-	signal(SIGKILL, sighandler); // kill
-	signal(SIGSEGV, sighandler); // segmentation violation
+  signal(SIGQUIT, sighandler); // Quit
+  signal(SIGILL,  sighandler); // Illegal instruction
+  signal(SIGKILL, sighandler); // kill
+  signal(SIGSEGV, sighandler); // segmentation violation
 
   unlink(SERVER_PATH); // Closing socket if already in use
 
-	/**
+  /**
 
-	@ Creating a socket.
+  @ Creating a socket.
 
-	- int
-	  socket(int domain, int type, int protocol);
-	- man 2 socket
+  - int
+    socket(int domain, int type, int protocol);
+  - man 2 socket
 
-	@ Arguments and their meaning.
+  @ Arguments and their meaning.
 
-	^ PF_LOCAL means the socket is for local communication,
-	  and is not a TCP socket for external communication.
+  ^ PF_LOCAL means the socket is for local communication,
+    and is not a TCP socket for external communication.
 
-	^ SOCK_STREAM makes the socket type 'stream'
-	  other types are datagram (udp) and
-	  raw (full control over internals)
-	  read the man pages
+  ^ SOCK_STREAM makes the socket type 'stream'
+    other types are datagram (udp) and
+    raw (full control over internals)
+    read the man pages
 
-	^ The last argument is for protocol.
-	  There is _usually_ just one protocol.
-	  0 is the first one. Arrays start with 0, right?
+  ^ The last argument is for protocol.
+    There is _usually_ just one protocol.
+    0 is the first one. Arrays start with 0, right?
 
-	^ Works on Macos (BSD, System V)
-	  May have to change the arguments for other systems.
-	  for example, AF_LOCAL. See man pages.
+  ^ Works on Macos (BSD, System V)
+    May have to change the arguments for other systems.
+    for example, AF_LOCAL. See man pages.
 
-	**/
+  **/
 
-	if ( (server_fd = socket(PF_LOCAL, SOCK_STREAM, 0)) == -1 ) {
-		perror("Socket creation failed");
-		exit(EXIT_FAILURE);
-	}
+  if ( (server_fd = socket(PF_LOCAL, SOCK_STREAM, 0)) == -1 ) {
+    perror("Socket creation failed");
+    exit(EXIT_FAILURE);
+  }
 
-	printf("%d\n", server_fd);
-
-
-	/**
-
-	@ making the socket receive multiple connections.
-
-	- int
-	  getsockopt(int socket, int level, int option_name, 
-	             void * option_value, socklen_t option_len);
-	- man 2 setsockopt
+  printf("%d\n", server_fd);
 
 
-	@ Arguments and their meaning.
-	
-	^ server_fd is the file descriptor for our socket
+  /**
 
-	^ SOL_SOCKET sets the level at which setsockopt should
-	  look for the option to change. Read man page.
+  @ making the socket receive multiple connections.
 
-	^ SO_REUSEADDR is the option we are going to change.
-	  This makes the socket address reusable.
-	  usually, when a client tries to connect to a socket
-	  _already_ in use, it results in an error.
+  - int
+    getsockopt(int socket, int level, int option_name, 
+               void * option_value, socklen_t option_len);
+  - man 2 setsockopt
 
-	  But we need multiple clients to connect to the server,
-	  hence we are reusing sockets
 
-	^ The value we are setting to the option SO_REUSEADDR
-	  is 1. But I can't pass this directly, the setsockopt function
-	  needs it as a pointer. So making an int = 1, and passing
-	  that int's pointer. That's int reuseAddress.
+  @ Arguments and their meaning.
+  
+  ^ server_fd is the file descriptor for our socket
 
-	**/
-	int reuseAddress = 1;
-	if( setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (void *)&reuseAddress,  sizeof(reuseAddress)) == -1 )   
+  ^ SOL_SOCKET sets the level at which setsockopt should
+    look for the option to change. Read man page.
+
+  ^ SO_REUSEADDR is the option we are going to change.
+    This makes the socket address reusable.
+    usually, when a client tries to connect to a socket
+    _already_ in use, it results in an error.
+
+    But we need multiple clients to connect to the server,
+    hence we are reusing sockets
+
+  ^ The value we are setting to the option SO_REUSEADDR
+    is 1. But I can't pass this directly, the setsockopt function
+    needs it as a pointer. So making an int = 1, and passing
+    that int's pointer. That's int reuseAddress.
+
+  **/
+  int reuseAddress = 1;
+  if( setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (void *)&reuseAddress,  sizeof(reuseAddress)) == -1 )   
     {   
         perror("An error occured when making the socket address reusable.");
 
         sighandler(EXIT_FAILURE);  
     }  
 
-    /**	
+    /** 
 
     @ Binding the socket to an address
 
@@ -155,38 +156,38 @@ int main() {
 
     ^ server_fd is the servers file descriptor.
 
-	^ We use the struct sockaddr_un to send these options to bind.
+  ^ We use the struct sockaddr_un to send these options to bind.
 
-		struct sockaddr_un {
-			unsigned short sun_family;
-			char sun_path[108];
-		}
+    struct sockaddr_un {
+      unsigned short sun_family;
+      char sun_path[108];
+    }
 
-	  I believe _un means unix.
-	  For TCP sockets, you'd send sockaddr_in. _in is Internet.
-	  probably.
+    I believe _un means unix.
+    For TCP sockets, you'd send sockaddr_in. _in is Internet.
+    probably.
 
-	  Similary, sun_family is socket unix family.
-	  It's sin_family for TCP sockets.
+    Similary, sun_family is socket unix family.
+    It's sin_family for TCP sockets.
 
-	  Anyhow, since these are local sockets and not TCP,
-	  we make sun_family PF_LOCAL
-	  and sun_path the file we want to bind to.
+    Anyhow, since these are local sockets and not TCP,
+    we make sun_family PF_LOCAL
+    and sun_path the file we want to bind to.
 
-	**/
+  **/
     struct sockaddr_un address;
 
     address.sun_family = PF_LOCAL;
     strcpy(address.sun_path, SERVER_PATH);
 
     if( bind(server_fd, (struct sockaddr *)&address, sizeof(address))  == -1) {
-    	perror("An error occured while binding the socket");
+      perror("An error occured while binding the socket");
       sighandler(EXIT_FAILURE);
     }
 
 
 
-    /**
+  /**
 
     @ Listening on the socket.
 
@@ -194,14 +195,14 @@ int main() {
       listen(int socket, int backlog);
     - man 2 listen
 
-	@ Arguments and their meaning.
+  @ Arguments and their meaning.
 
-	^ server_fd is the server file descriptor.
+  ^ server_fd is the server file descriptor.
     ^ MAX_SIMULTANEOUS_CONNECTIONS is what it says.
 
 
     **/
-	if ( listen(server_fd, MAX_SIMULTANEOUS_CONNECTIONS) == -1 ) {   
+  if ( listen(server_fd, MAX_SIMULTANEOUS_CONNECTIONS) == -1 ) {   
         perror("An error occured while listening in server");  
         sighandler(EXIT_FAILURE);    
     }
@@ -220,16 +221,16 @@ int main() {
 
     while(1) {
 
-    	/**
+      /**
 
-    	@ Accepting connections on the socket.
+      @ Accepting connections on the socket.
 
-    	- int
-    	  accept(int socket, struct sockaddr address,
-    	         socklen_t address_len);
-    	- man 2 accept
+      - int
+        accept(int socket, struct sockaddr address,
+               socklen_t address_len);
+      - man 2 accept
 
-    	accept() extracts the first connection request on the queue
+      accept() extracts the first connection request on the queue
         of pending connections, creates a new socket with the same 
         properties of socket, and allocates a new file
         descriptor for the socket.
@@ -244,24 +245,24 @@ int main() {
         ^ address_len has the size of the second
           argument, in bytes.
 
-		We don't really need the clients address and such.
-		Just the file descriptor is plenty. So passing NULL.
+    We don't really need the clients address and such.
+    Just the file descriptor is plenty. So passing NULL.
 
 
-    	**/
-    	int client_fd;
-    	if((client_fd = accept(server_fd, NULL, NULL)) == -1) {
-    		perror("Error accepting connection from client.");
-    		// exit(EXIT_FAILURE);
-    		// Not exiting because there might be other connections.
-    	}
+      **/
+      int client_fd;
+      if((client_fd = accept(server_fd, NULL, NULL)) == -1) {
+        perror("Error accepting connection from client.");
+        // exit(EXIT_FAILURE);
+        // Not exiting because there might be other connections.
+      }
 
-    	char letter;
-    	if(read(client_fd, &letter, sizeof(char)) == -1) {
-    		perror("Error reading from client.");
-    	}
+      char letter;
+      if(read(client_fd, &letter, sizeof(char)) == -1) {
+        perror("Error reading from client.");
+      }
 
-    	printf("From client: %c\n", letter);
+      printf("From client: %c\n", letter);
 
     }
 
